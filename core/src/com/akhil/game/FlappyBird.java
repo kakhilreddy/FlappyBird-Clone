5f0b4598d99fb478e50a0b97875f15cadc4a2128
Akhil Reddy <kakhilreddy1996@gmail.com>
2016-09-15 21:50:08 +0530

    Update FlappyBird.java

--- core/src/com/akhil/game/FlappyBird.java
+++ core/src/com/akhil/game/FlappyBird.java
@@ -33,44 +33,45 @@ import java.util.Random;
 
 
 public class FlappyBird extends ApplicationAdapter {
+	
 	SpriteBatch batch;
-	Texture back;
-	Texture birds[];
-	Texture bottom;
-	int flappyState=0;
-	int score=0;
-	float birdY=0;
-	float velocity=0;
-	int gameState=0;
-	float gravity=2;
-	float gap=475;
-	Texture gameOver;
+	Texture back; ///background image
+	Texture birds[]; // bird images
+	Texture bottom; //Bottom tube
+	int flappyState=0; // state of bird
+	int score=0; // initial score
+	float birdY=0; // y coordinate of bird
+	float velocity=0; // velocity of bird
+	int gameState=0; //initial game state is 0 means game didnt start
+	float gravity=2; 
+	float gap=475; //gap between top and bottom tube
+	Texture gameOver; //game over image
 	BitmapFont font;
-	Texture top;
-	int touch=0;
-	float maxTubeOffset;
-Circle birdCircle;
-	int noOfTubes=4;
-	Sound jump,sc;
-	float distanceBetweenTubes;
-	float tubeVelocity=4;
-	float []tubeX=new float[noOfTubes];
-	float []tubeOffset=new float[noOfTubes];
+	Texture top; //Top tube 
+	int touch=0; //used when game is over
+	float maxTubeOffset; 
+Circle birdCircle;  
+	int noOfTubes=4;// no of tubes
+	Sound jump,sc; //sound effects
+	float distanceBetweenTubes; // distance between consecutive tubes
+	float tubeVelocity=4;  //speed of tubes
+	float []tubeX=new float[noOfTubes]; // since Y coordinate remains same so we take array of X coordinates
+	float []tubeOffset=new float[noOfTubes]; // max height of tube
 	Rectangle[]topTubeRectangles;
 	Rectangle[]bottomTubeRectangles;
-	int scoreTube=0;
-	Texture scorecard;
-	boolean play=false;
-Music over;
-	int highScore=0;
-ShapeRenderer shapeRenderer;
-	Random random;
-	Music music;
-	private boolean flag=false;
+	int scoreTube=0; // keeping track of score being earned from respective tube
+	Texture scorecard; // To be displayed after game is over
+	boolean play=false; //related to playing music when game is over
+Music over; // game over music
+	int highScore=0; //highScore scored till now , obtained by using Preferences class
+
+	Random random; // random no generator so that tubes are of different sizes
+	Music music; // background music
+	private boolean flag=false; // related to game over state
 
 	@Override
 	public void create () {
-		shapeRenderer=new ShapeRenderer();
+	///initializing fields
 		birdCircle=new Circle();
 		font=new BitmapFont();
 		scorecard=new Texture("scorecard1.png");
@@ -103,7 +104,7 @@ startGame();
 	{
 		birdY=Gdx.graphics.getHeight()/2-birds[flappyState].getHeight()/2;
 		for(int i=0;i<noOfTubes;i++)
-		{
+		{ // initialising tubes offset and X coordinate of both bottom and top tubes
 			tubeOffset[i]=(random.nextFloat()-0.5f)*(Gdx.graphics.getHeight()-gap-200);
 
 			tubeX[i]=Gdx.graphics.getWidth()/2-top.getWidth()/2+Gdx.graphics.getWidth()+ i*distanceBetweenTubes;
@@ -119,12 +120,15 @@ startGame();
 	@Override
 	public void render () {
 		batch.begin();
+		//drawing background image
 		batch.draw(back,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
-
+//gameState =1 means game is in progress
+//gameState= 2 means game over
+//gameState =3 means game didnt start ,waiting for user to touch the screen  
 		if(gameState==1)
-		{
-			if(tubeX[scoreTube]<Gdx.graphics.getWidth()/2)
+		{                  
+			if(tubeX[scoreTube]<Gdx.graphics.getWidth()/2) //if user successfully passes through the tube
 			{
 				sc.play();
 				score++;
@@ -136,29 +140,31 @@ startGame();
 			}
 
 
-			if(Gdx.input.justTouched())
-		{velocity=-30;
+			if(Gdx.input.justTouched()) // if user touches screen 
+		{velocity=-30; // setting velocity so that it will look  like bird is jumping
 jump.play();
 		}
 			for(int i=0;i<noOfTubes;i++) {
-				if(tubeX[i]<-top.getWidth())
-				{
+			
+				if(tubeX[i]<-top.getWidth()) // if x coordinate is less than negative of tube width means the tube passed the left most end of the phone screen
+				{ // so reset the tube x coordinate so that it shifts back to rightmost end of the phone screen
 					tubeX[i]+=noOfTubes*distanceBetweenTubes;
 					tubeOffset[i]=(random.nextFloat()-0.5f)*(Gdx.graphics.getHeight()-gap-200);
-				}else{
+				}else{ // or else decrement x coordinate ,it looks like tube is moving from right to left
 				tubeX[i] -= tubeVelocity;
 
 
 				}
+				//draw the tubes
 				batch.draw(top, tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 +tubeOffset[i]);
 				batch.draw(bottom, tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottom.getHeight() + tubeOffset[i]);
                      topTubeRectangles[i]=new Rectangle( tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 +tubeOffset[i],top.getWidth(),top.getHeight());
 				bottomTubeRectangles[i]=new Rectangle( tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottom.getHeight() + tubeOffset[i],bottom.getWidth(),bottom.getHeight());
-
-			}if(birdY>0 )
+//Rectanges objects are used so that we can detect collisions
+			}if(birdY>0 ) // if y coordinate of bird is greater than zero ,then decrement the y coordinate
 			{velocity+=gravity;
 		birdY-=velocity;}else
-		{
+		{ // else bird is at bottom of screen so game is over ,set the gameState to 2
 			gameState=2;
 			if(!play)
 			over.play();
@@ -167,33 +173,34 @@ jump.play();
 
 
 		}else if(gameState==0) {
-
+//gameState is 0,waiting for users touch to start the game
 
 			if(Gdx.input.justTouched())
-			{
+			{ //start the game
 				gameState=1;
 				Gdx.app.log("touched","touch");
 			}
 
 		}else if(gameState==2)
 		{
-
+//game is over so displaying gameOver image and scorecard
 
 			batch.draw(gameOver,Gdx.graphics.getWidth()/2-gameOver.getWidth()/2,Gdx.graphics.getHeight()/2-gameOver.getHeight()/2);
 			batch.draw(scorecard,Gdx.graphics.getWidth()/2-scorecard.getWidth()/2,Gdx.graphics.getHeight()/2+gameOver.getHeight()/2+100);
            font.getData().setScale(2);
 			font.draw(batch,String.valueOf(score),Gdx.graphics.getWidth()/2-60,Gdx.graphics.getHeight()/2+270);
-			Preferences preferences=Gdx.app.getPreferences("highscore");
+			Preferences preferences=Gdx.app.getPreferences("highscore"); //getting the value of highscore
 			highScore=preferences.getInteger("highscore",0);
 
-			if(score>highScore)
+			if(score>highScore)// comparing current score with highscore
 			{
 				preferences.putInteger("highscore",score);
-				highScore=score;
+				highScore=score; // setting highscore as score
 				preferences.flush();
 			}
 			font.draw(batch,String.valueOf(highScore),Gdx.graphics.getWidth()/2-60,Gdx.graphics.getHeight()/2+200);
 //Gdx.app.log("birdy",String.valueOf(birdY));
+//bird falls down when game is over
 			if(birdY>0)
 			{gravity=5;
 			velocity+=gravity;
@@ -213,6 +220,7 @@ play=true;
 			if(Gdx.input.justTouched() && flag)
 						{ touch++;
 							if(touch>1){gameState=1;
+							//user touched the screen again , so restart the game 
 				startGame();
 			score=0;
 play=false;
@@ -222,7 +230,7 @@ play=false;
 					touch=0;		}
 		}}
 
-
+//to display animation of flipping wings of bird ,flappyState is used to set bird image
 		  if (flappyState == 0) {
 			  flappyState = 1;
 		  } else
@@ -233,11 +241,12 @@ play=false;
 		  font.draw(batch, String.valueOf(score), 100, 200);
 
 		  batch.end();
+		  //Circle shape size of bird is created
 		  birdCircle.set(Gdx.graphics.getWidth() / 2, birdY + birds[flappyState].getHeight() / 2, birds[flappyState].getWidth() / 2);
 
 		  for (int i = 0; i < noOfTubes; i++) {
 
-
+//If tube(rectangle) and bird(circle) interesects then game is over
 			  if (Intersector.overlaps(birdCircle, topTubeRectangles[i]) || Intersector.overlaps(birdCircle, bottomTubeRectangles[i])) {
 
 				  Gdx.app.log("collision", "yes");
@@ -250,7 +259,7 @@ play=false;
 
 
 
-	
+	//clearing up memory
 	@Override
 	public void dispose () {
 		batch.dispose();
